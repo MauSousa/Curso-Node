adc82ada548baaa2b0b6b364e42c70eb087fe071
Augusto <lagrange.tesla@gmail.com>
2021-03-08 20:28:57 -0600

    Se vio el tema de funciones de flecha

---
+++ 02-fundamentos/flecha.js
@@ -0,0 +1,9 @@
+// Funcion tradicional
+function sumar(a,b) {
+    return a + b
+}
+console.log( sumar(5,10) )
+
+// Funcion de flecha
+const sumarFlecha = (a,b) => a + b
+console.log( sumarFlecha(5,11) )
