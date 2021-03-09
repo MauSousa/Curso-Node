f5e6ce4a657e0c6b6f203658bffdeb26340a2a1c
Augusto <lagrange.tesla@gmail.com>
2021-03-08 20:03:30 -0600

    Se vio el tema de template strings

---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,8 @@
+const nombre = 'Deadpool'
+const nombreReal = 'Wade Winston'
+
+const concatenacionNormal = nombre + ' ' + nombreReal
+console.log( concatenacionNormal )
+
+const template = `${ nombre } ${nombreReal}`
+console.log( template )
