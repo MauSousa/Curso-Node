ea471843d89059a8e56f604355fcfb0a353669ad
Augusto <lagrange.tesla@gmail.com>
2021-03-12 10:09:27 -0600

    Se vieron los parametros en consola de una forma que no se debe hacer

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,22 +1,17 @@
 
 const { crearArchivo } = require('./helpers/multiplicar.js')
 
-// Imprimir la tabla del cinco
-/*const tablaDelNumero = ( numero ) => {
-  
-  console.clear();
-  console.log(`Imprimiendo la tabla del ${numero}`)
-  for(let num = 1; num <= 10; num++) {
-    let resultado = numero * num
-    console.log(`${numero} * ${num} = ${resultado}`)
-  }
-
-}
-tablaDelNumero(5)*/
-
 console.clear()
 
-const base = 1
+// console.log( process.argv )
+
+// Esto no se hace
+// const [ , ,argumentoTres ] = process.argv
+// const [, base = 5 ] = argumentoTres.split('=')
+
+// console.log( base )
+
+// const base = 1
 
 crearArchivo( base )
   .then(nombreArchivo => console.log( nombreArchivo, 'creado'))
