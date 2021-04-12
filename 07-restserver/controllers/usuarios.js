8a9f0c692534703012c02ce743c2a6c0ea79c9f1
Augusto <lagrange.tesla@gmail.com>
2021-04-12 17:29:56 -0500

    Fin del RestServer basico

--- 07-restserver/controllers/usuarios.js
+++ 07-restserver/controllers/usuarios.js
@@ -1,20 +1,32 @@
 const { response } = require('express')
 
 const usuariosGet = (req, res = response) => {
+  const { query = 'No query', nombre, apiKey } = req.query
+
   res.json({
     message: 'Get API - Controlador',
+    query,
+    nombre,
+    apiKey,
   })
 }
 
 const usuariosPost = (req, res = response) => {
-  res.status(201).json({
+  const { nombre, edad } = req.body
+
+  res.json({
     message: 'POST API - Controlador',
+    nombre,
+    edad,
   })
 }
 
 const usuariosPut = (req, res = response) => {
+  const { id } = req.params
+
   res.json({
     message: 'Put API - Controlador',
+    id,
   })
 }
 
