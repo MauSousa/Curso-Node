3bf0a1c4523e451e3eb7f6254ee7946ae869e9bc
Augusto Sousa <lagrange.tesla@gmail.com>
2022-02-09 11:29:07 -0600

    Middlewares agregados

--- 11-ts-restserver/models/server.ts
+++ 11-ts-restserver/models/server.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 import usersRoutes from '../routes/user'
+import cors from 'cors';
 
 class Server {
 
@@ -10,9 +11,27 @@ class Server {
   }
 
   constructor() {
+
     this.app = express();
     this.port = process.env.PORT || '8000';
+    this.middlewares();
     this.routes();
+  
+  }
+
+  // TODO Connect to db
+
+  middlewares(){
+    
+    // CORS
+    this.app.use( cors() );
+    
+    // body parsing
+    this.app.use( express.json() );
+    
+    // Public directory 
+    this.app.use( express.static('public') );
+  
   }
 
   routes() {
